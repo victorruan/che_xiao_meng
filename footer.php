5ad423ded467e3973d0ba5d4c44a8ca3266b97a1
阮佳佳 <250069802@qq.com>
2016-09-19 18:21:41 +0800

    ~

--- footer.php
+++ footer.php
@@ -34,4 +34,16 @@
         }
     });
 </script>
+<script>
+    var _hmt = _hmt || [];
+    (function() {
+        var hm = document.createElement("script");
+        hm.src = "//hm.baidu.com/hm.js?078142a342fbe7742cc4212b2892eda0";
+        var s = document.getElementsByTagName("script")[0];
+        s.parentNode.insertBefore(hm, s);
+    })();
+
+    var cnzz_protocol = (("https:" == document.location.protocol) ? " https://" : " http://");
+    document.write(unescape("%3Cspan id='cnzz_stat_icon_1259827081'%3E%3C/span%3E%3Cscript src='" + cnzz_protocol + "s4.cnzz.com/z_stat.php%3Fid%3D1259827081' type='text/javascript'%3E%3C/script%3E"));
+</script>
 </html>
